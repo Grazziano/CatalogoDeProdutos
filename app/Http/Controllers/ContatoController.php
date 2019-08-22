4cedac6926c8a870e60e33881d498f22cf054cff
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2019-08-22 11:01:12 -0300

    Criando método de envio de e-mails

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mail;
 
 class ContatoController extends Controller
 {
@@ -11,5 +12,23 @@ class ContatoController extends Controller
         $data['titulo'] = "Contato";
 
         return view('contato', $data);
+    }
+
+    public function enviar(Request $request)
+    {
+        $dadosEmail = array(
+            'nome' => $request->input('nome'),
+            'email' => $request->input('email'),
+            'assunto' => $request->input('assunto'),
+            'msg' => $request->input('msg'),
+        );
+
+        Mail::send('email.contato', $dadosEmail, function ($message) {
+            $message->from('formulario@pontocomdesenvolvimento.net', 'Formulário de contato');
+            $message->subject('Mensagem enviada pelo formulário de contato');
+            $message->to('grazzianofagundes@gmail.com');
+        });
+
+        return redirect('contato')->with('success', 'Mensagem enviada com sucesso! Entraremos em contato em breve!');
     }
 }
